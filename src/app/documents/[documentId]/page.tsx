cd747de55c2ce8e45c3533a02c406ce1725aebff
aarsh21 <aarshpersonal@proton.me>
2025-09-14 14:55:39 +0530

    feat: add global store to make toolbar

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -3,11 +3,13 @@ interface DocumentIdPageProps {
 }
 
 import { Editor } from "./editor";
+import { Toolbar } from "./toolbar";
 
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
   const { documentId } = await params;
   return (
     <div className="min-h-screen bg-[#fafbfd]">
+      <Toolbar />
       <Editor />
     </div>
   );
